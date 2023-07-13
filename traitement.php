7f11cae323acde86825edf59c722aec7728c4e67
derdeche <foued.derdeche@gmail.com>
2023-07-13 16:45:43 +0200

    modif syntaxe

--- traitement.php
+++ traitement.php
@@ -4,8 +4,8 @@ session_start();
 if(isset($_POST['submit'])){
 
     $name = filter_input(INPUT_POST, "name" , FILTER_SANITIZE_STRING);
-    $price = filter_inut(INPUT_POST, "price" , FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
-    $qtt = filter_input (INPUT_POST,"qtt",FILTER_VALIDATE_INT).
+    $price = filter_input(INPUT_POST, "price" , FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+    $qtt = filter_input (INPUT_POST,"qtt",FILTER_VALIDATE_INT);
 
     if($name && $price && $qtt){
 
