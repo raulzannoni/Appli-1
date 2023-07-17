8fcede4cb28c952ccb469ff26a4fb0483ee993c9
derdeche <foued.derdeche@gmail.com>
2023-07-18 01:56:03 +0200

    rajout bouton voir panier et commentaire

--- index.php
+++ index.php
@@ -4,32 +4,59 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ajout produit</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    
+    
+    <title>Ajout produit</title>
+    
 </head>
-<body>
-
-    <h1>Ajouter un produit</h1>
-    <form action="traitement.php" method="post">
+<body >
+    <div class="d-flex p-2" >
+        <h1 class="p-2 mb-2 bg-primary text-white ">Ajouter un produit</h1>
+        
+</div>
+    
+    <form action="traitement.php?action=add" method="post">
+    <?php 
+    session_start();
+    
+        if(empty($_SESSION["products"]))
+        {echo "Il n'y a aucun produit dans le panier";}
+        else{ echo "Produit Ajouté ". " Il y a ". count($_SESSION["products"])." produits dans le panier";}            
+                           
+    ?>
+    
         <p>
-            <label>
-                Nom du produit :
+            <label class="p-3 mb-2 form-label">
+                Nom du produit : <br>
                 <input type="text" name="name">
             </label>
         </p>
         <p>
-            <label>
-                Prix du produit :
+            <label class="p-3 mb-2 form-label">
+                Prix du produit : <br>
                 <input type="number" step="any" name="price">
             </label>
         </p>
         <p>
-            <label>
-                Quantité désirée :
+            <label class="p-3 mb-2 form-label">
+                 Quantité désirée : <br>
                 <input type="number" name="qtt" value="1">
             </label>
         </p>
         <p>
-            <input type="submit" name="submit" value="Ajouter le produit">
+            <label class="p-3 mb-2 form-label">
+            <input type="submit" name="submit" value="Ajouter le produit" class="p-3 mb-2 bg-primary text-white">
+            </label>
         </p>
+        </div>
     </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
+
+<footer>
+    <label class="p-3 mb-2 form-label">
+        <a href="recap.php" class="p-3 mb-2 bg-success text-white">Voir le panier</a> 
+    </label>
+</footer>
 </html>
