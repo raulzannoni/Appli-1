73493dc0526d504b5108c9a0404f5dd2d527c6af
derdeche <foued.derdeche@gmail.com>
2023-07-18 13:58:44 +0200

    rajout ob_start

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ajout produit</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-        
-    <title>Ajout produit</title>
-    
-</head>
-<body >
+<?php
+
+ob_start()
+
+?>
+
     <div class="d-flex p-2" >
         <h1 class="p-2 mb-2 bg-primary text-white ">Ajouter un produit</h1>
         
@@ -51,12 +45,15 @@
         </p>
         </div>
     </form>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-</body>
-
-<footer>
+    <footer>
     <label class="p-3 mb-2 form-label">
         <a href="recap.php" class="p-3 mb-2 bg-success text-white">Voir le panier</a> 
     </label>
-</footer>
-</html>
+    </footer>
+  
+
+    <?php
+    $titre = "Ajout produit";
+    $contenu = ob_get_clean();
+
+    require_once "template.php";
