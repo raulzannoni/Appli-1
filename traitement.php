08cf27a7862c87b915456394a373034a028c80b3
derdeche <foued.derdeche@gmail.com>
2023-07-18 09:30:20 +0200

    reglage

--- traitement.php
+++ traitement.php
@@ -2,29 +2,29 @@
 session_start();
 
 switch ($_GET['action']){
-    case 'add';
-        if(isset($_POST['submit'])){
+        case 'add';
+            if(isset($_POST['submit'])){
 
-            $name = filter_input(INPUT_POST, "name" , FILTER_SANITIZE_STRING);
-            $price = filter_input(INPUT_POST, "price" , FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
-            $qtt = filter_input (INPUT_POST,"qtt",FILTER_VALIDATE_INT);
+                $name = filter_input(INPUT_POST, "name" , FILTER_SANITIZE_STRING);
+                $price = filter_input(INPUT_POST, "price" , FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+                $qtt = filter_input (INPUT_POST,"qtt",FILTER_VALIDATE_INT);
 
-        if($name && $price && $qtt){
+            if($name && $price && $qtt){
 
-            $product = [
-                "name" =>$name,
-                "price"=> $price,
-                "qtt" =>$qtt,
-                "total" => $price*$qtt
-                        ];
+                $product = [
+                    "name" =>$name,
+                    "price"=> $price,
+                    "qtt" =>$qtt,
+                    "total" => $price*$qtt
+                            ];
 
-        $_SESSION['products'][] = $product;
-        
-    }
-}
+            $_SESSION['products'][] = $product;
+                    
+                }
+            }
 
 
-header("Location:index.php"); 
+        header("Location:index.php"); 
 		break;
 
         /* action de supprimer tout le panier*/
